4bbd0a51bda16e8daa50d81078b6d1be1be3c932
briancosentino <cosentinobrian@gmail.com>
2025-04-16 12:52:57 +0200

    add fetching movies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
+    const [movies, setMovies] = useState(null)
+    const fetchMovies = () => {
+        fetch('http://localhost:3002/movies/')
+            .then(res => res.json())
+            .then(data => setMovies(data))
+            .catch(err => console.error(err))
+    }
+    useEffect(() => {
+        fetchMovies()
+    }, [])
+
+    console.log(movies);
+
     return (
         <div>
             Home
